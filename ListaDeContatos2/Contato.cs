5fb4ca51b92cc01092fb29345962998291f05deb
Vinicius Mussak <vinicius.mussak@outlook.com>
2026-04-08 21:41:50 -0300

    contato

--- ListaDeContatos2/Contato.cs
+++ ListaDeContatos2/Contato.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,6 +10,8 @@ namespace ListaDeContatos2
         public string Telefone { get; set; }
         public string Tipo { get; set; }
 
+        public string MensagemErro { get; set; }
+
         public string Mostrar()
         {
             return $"{Nome} possui o {Tipo}: {Telefone}";
@@ -17,8 +19,59 @@ namespace ListaDeContatos2
 
         public bool PossuiTelefoneValido()
         {
-            //Telefone com 9 caracteres é valido
+            if (Telefone.Length != 11)
+                return false;
+
+            var somenteNumeros = long.TryParse(Telefone, out _);
+            if(!somenteNumeros)
+                return false;
+
+            return true;
+        }
+
+        public bool PossuiNomeValido()
+        {
+            if (Nome.Length == 0)
+                return false;
+
+            if (Nome.Length > 15)
+                return false;
+
+            return true;
+        }
+
+        public bool PossuiTipoValido()
+        {
+            if (Tipo == "Celular")
+                return true;
+
+            if (Tipo == "Fixo")
+                return true;
+
             return false;
+        }
+
+        public bool Validar()
+        {
+            if (!PossuiTelefoneValido())
+            {
+                MensagemErro = "Telefone inválido";
+                return false;
+            }
+
+            if (!PossuiNomeValido())
+            {
+                MensagemErro = "Nome inválido";
+                return false;
+            }
+
+            if (!PossuiTipoValido())
+            {
+                MensagemErro = "Tipo inválido";
+                return false;
+            }
+
+            return true;
         }
     }
 }
